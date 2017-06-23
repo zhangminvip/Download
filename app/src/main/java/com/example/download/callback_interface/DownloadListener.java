1fcbba24bc65f2e082ec35769943f7e38576709c
zhangminvip <2107609379@qq.com>
2017-06-23 23:08:02 +0800

    Define a callback interface,Used to monitor and callback various states in the download

--- app/src/main/java/com/example/download/callback_interface/DownloadListener.java
+++ app/src/main/java/com/example/download/callback_interface/DownloadListener.java
@@ -4,6 +4,15 @@ package com.example.download.callback_interface;
  * Created by tom on 2017/6/23.
  */
 
-public class DownloadListener {
-    
+public interface DownloadListener {
+
+    void onProgress(int progress);
+
+    void onSuccess();
+
+    void onFailed();
+
+    void onPaused();
+
+    void onCanceled();
 }
